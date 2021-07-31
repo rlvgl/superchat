4eb8ad509fa20108879b7d1845f5438a686f6dc2
Sohum Agrawal <sohum@Sohums-Air.attlocal.net>
2021-07-31 14:29:55 -0700

    finished project for the most part. Still some issue with firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+	apiKey: 'AIzaSyAYfrsG5TocCHayK6C8MTqwGaGEwVb58dU',
+	authDomain: 'superchat-72fb9.firebaseapp.com',
+	projectId: 'superchat-72fb9',
+	storageBucket: 'superchat-72fb9.appspot.com',
+	messagingSenderId: '956097397285',
+	appId: '1:956097397285:web:66d051a7522752792b35cc',
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+console.log(firebase);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [user] = useAuthState(auth);
+
+	return (
+		<div className='App'>
+			{/* <header className='App-header'>SuperChat</header> */}
+			{user ? <Chat /> : <SignIn />}
+			<SignOut />
+		</div>
+	);
 }
+
+const SignIn = () => {
+	const signInWithGoogle = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		auth.signInWithPopup(provider);
+	};
+
+	return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+};
+
+const SignOut = () => {
+	return (
+		auth.currentUser && (
+			<button onClick={() => auth.signOut()}>Sign Out</button>
+		)
+	);
+};
+
+const Chat = () => {
+	const messagesRef = firestore.collection('messages');
+	const query = messagesRef.orderBy('createdAt').limit(25);
+
+	const [messages] = useCollectionData(query, { idField: 'id' });
+
+	const [message, setMessage] = useState('');
+
+	const handleMessageChange = (e) => {
+		setMessage(e.target.value);
+	};
+
+	const handleSendMessage = () => {
+		messagesRef.add({
+			text: message,
+			createdAt: new Date(),
+		});
+		setMessage('');
+	};
+
+	return (
+		<>
+			<h1>This is the Chat Room</h1>
+			<div>
+				{messages &&
+					messages.map((msg) => (
+						<ChatMessage message={msg} key={msg.id} />
+					))}
+
+				<input
+					type='text'
+					onChange={handleMessageChange}
+					value={message}
+				></input>
+				<input type='submit' onClick={handleSendMessage} />
+			</div>
+		</>
+	);
+};
+
+const ChatMessage = (props) => {
+	const { text } = props.message;
+	console.log(text);
+
+	return <p>{text}</p>;
+};
 
 export default App;
